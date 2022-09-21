55f2327314fcc296affaac6ae613bb1bcf834860
wangping984 <97883369+wangping984@users.noreply.github.com>
2022-09-21 16:53:16 +0800

    daily commit

--- hbte_eval.py
+++ hbte_eval.py
@@ -1,6 +1,8 @@
 from hbte import PA
 import time
 import pyvisa as visa
+import sys
+
 
 def hbte_eval_deg():
     pa = PA("192.168.1.254")
@@ -15,23 +17,27 @@ def hbte_eval_deg():
     N5244A.write(":TRIGger:SEQuence:SOURce %s" % ("MANual"))
     N5244A.write(":INITiate:CONTinuous %d" % (0))
     N5244A.write(":SOURce:POWer:LEVel:IMMediate:AMPLitude %G" % (10.0))
+    # hbte.csa has a meas named meas1
+    N5244A.write(':CALCulate:PARameter:SELect "%s"' % ("meas1"))
 
     for deg in range(360):
         pa.operateChannelPhase(channelList=[ch_num], phaseList=[deg])
         time.sleep(0.1)
         N5244A.write(":INITiate:IMMediate")
         # 401 points, 1khz ifbw, 0.4s*2 =0.8s at least to sweep
-        time.sleep(0.8)
+        time.sleep(1.2)
         N5244A.write("*WAI")
         N5244A.write(
             ':CALCulate:DATA:SNP:PORTs:SAVE "%s","%s"'
-            % ("1,2", "D:\\20220920\\A1B1_loop2_deg" + str(deg) + ".s2p")
+            % ("1,2", "D:\\20220921\\A1B1_r7_deg" + str(deg) + ".s2p")
         )
         N5244A.write("*WAI")
-
+        sys.stdout.write("Test progress: %d  of 359 deg \r" % (deg))
+        sys.stdout.flush()
 
     N5244A.close()
     rm.close()
+
 
 def hbte_eval_mag():
     pa = PA("192.168.1.254")
@@ -46,23 +52,27 @@ def hbte_eval_mag():
     N5244A.write(":TRIGger:SEQuence:SOURce %s" % ("MANual"))
     N5244A.write(":INITiate:CONTinuous %d" % (0))
     N5244A.write(":SOURce:POWer:LEVel:IMMediate:AMPLitude %G" % (10.0))
+    N5244A.write(':CALCulate:PARameter:SELect "%s"' % ("meas1"))
 
     for mag in range(180):
-        pa.operateChannel(channelList=[ch_num], valueList=[mag/2])
+        pa.operateChannel(channelList=[ch_num], valueList=[mag / 2])
         time.sleep(0.1)
         N5244A.write(":INITiate:IMMediate")
         # 401 points, 1khz ifbw, 0.4s*2 =0.8s at least to sweep
-        time.sleep(0.8)
+        time.sleep(1.2)
         N5244A.write("*WAI")
         N5244A.write(
             ':CALCulate:DATA:SNP:PORTs:SAVE "%s","%s"'
-            % ("1,2", "D:\\20220920\\A1B1_mag" + str(mag/2) + ".s2p")
+            % ("1,2", "D:\\20220921\\A1B1_r6_mag" + str(mag / 2) + ".s2p")
         )
         N5244A.write("*WAI")
-
+        sys.stdout.write("Test progress: %.1f  of 90 dB \r" % (mag/2))
+        sys.stdout.flush()
 
     N5244A.close()
     rm.close()
 
-if __name__ == '__main__':
-    hbte_eval_mag()
+
+if __name__ == "__main__":
+    # hbte_eval_mag()
+    hbte_eval_deg()
